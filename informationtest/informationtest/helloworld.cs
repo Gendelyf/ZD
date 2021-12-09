215daabe4977a312e38cd2890ef36793a3d7fcaf
Gendelyf <80451266+Gendelyf@users.noreply.github.com>
2021-12-09 11:33:45 +0300

    Add files via upload

---
+++ informationtest/informationtest/helloworld.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace informationtest
+{
+    public partial class helloworld : Form
+    {
+        public helloworld()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.Cursor = Cursors.Hand;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            zd1 z1 = new zd1();
+            this.Hide();
+            z1.ShowDialog();
+            this.Show();
+            helloworld hell = new helloworld();
+            hell.Hide();
+
+        }
+    }
+}
